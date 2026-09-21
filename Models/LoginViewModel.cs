09b4fdbf74e569a447a84560e59c9409cffeb324
yhostinnapa0-cloud <yhostinnapa0@gmail.com>
2026-09-21 15:57:10 -0500

    La carpeta MODELO

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoStore.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Ingrese su correo electrónico.")]
+        [EmailAddress(ErrorMessage = "Ingrese un correo válido.")]
+        public string Email { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Ingrese su contraseña.")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
